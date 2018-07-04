5ca092fef428eee351f3c9906338da4e645cdddf
gremlin <gremlin@portable.u5i5.com>
2018-07-04 20:41:51 +0800

    Add (BytesAppender).Reset().

--- xstr/bytesappender.go
+++ xstr/bytesappender.go
@@ -4,13 +4,16 @@ type BytesAppender struct {
 	buf *[]byte
 }
 
+// NewBytesAppender will reset the buffer pos to the first byte
 func NewBytesAppender(buf *[]byte) BytesAppender {
 	if buf == nil {
 		buf = new([]byte)
 	}
+	*buf = (*buf)[:0]
 	return BytesAppender{buf}
 }
 
+// Write implements io.Writer interface
 func (ba BytesAppender) Write(data []byte) (int, error) {
 	if len(data) > 0 {
 		*ba.buf = append(*ba.buf, data...)
@@ -18,6 +21,12 @@ func (ba BytesAppender) Write(data []byte) (int, error) {
 	return len(data), nil
 }
 
+// Reset set the buffer pos to the first byte
+func (ba BytesAppender) Reset() {
+	*ba.buf = (*ba.buf)[:0]
+}
+
+// Bytes returns the underlying buffer
 func (ba BytesAppender) Bytes() []byte {
 	return *ba.buf
 }
