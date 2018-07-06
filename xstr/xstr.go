0bd66136b2eacc998bcca78dfe0af30f5be4a497
gremlin <gremlin@portable.u5i5.com>
2018-07-06 11:09:21 +0800

    Add func RightAlignedCopy()

--- xstr/xstr.go
+++ xstr/xstr.go
@@ -5,6 +5,18 @@ import (
 	"strings"
 )
 
+func RightAlignedCopy(to []byte, from []byte) {
+	k := len(to) - len(from)
+	if k > 0 {
+                for i := 0; i < k; k++ {
+                        to[i] = 0
+                }
+                copy(to[k:], from)
+	} else {
+		k = -k
+		copy(to, from[k:])
+	}
+}
 
 func SplitKeyValue(s string, sep string) (key, value string) {
 	i := strings.Index(s, sep)
