e61140f53371d305a9d375954ffa26b0248f413f
gremlin <gremlin@u5i5.com>
2018-08-28 10:50:42 +0800

    Rename eax.NewCMAC() to eax.NewCmac()

--- eax/eax.go
+++ eax/eax.go
@@ -16,9 +16,9 @@ type CmacCtx struct {
 	count int
 }
 
-func NewCMAC(cipher cipher.Block) (*CmacCtx, error) {
+func NewCmac(cipher cipher.Block) (*CmacCtx, error) {
 	if cipher.BlockSize() != BLOCK_SIZE {
-		return nil, errors.New("eax: NewCMAC requires 128-bit block cipher")
+		return nil, errors.New("eax: NewCmac requires 128-bit block cipher")
 	}
 	return &CmacCtx{cipher:cipher}, nil
 }
@@ -45,67 +45,67 @@ func (ca *CmacCtx) Update(buf []byte) {
 	i := 0
 	num := len(buf)
 	for i < num && k < BLOCK_SIZE {
-                ca._X[k] ^= buf[i]
+		ca._X[k] ^= buf[i]
 		i++
 		k++
 	}
 
 	num -= i
-        ca.count += i
+	ca.count += i
 	for num >= BLOCK_SIZE {
-                ca.cipher.Encrypt(ca._X[:], ca._X[:])
-                blockXOR(ca._X[:], buf[i:])
-                i += BLOCK_SIZE
-                num -= BLOCK_SIZE
-                ca.count += BLOCK_SIZE
-        }
+		ca.cipher.Encrypt(ca._X[:], ca._X[:])
+		blockXOR(ca._X[:], buf[i:])
+		i += BLOCK_SIZE
+		num -= BLOCK_SIZE
+		ca.count += BLOCK_SIZE
+	}
 
 	if num > 0 {
-                ca.cipher.Encrypt(ca._X[:], ca._X[:])
-                for k = 0; k < num; k++ {
-                        ca._X[k] ^= buf[i]
+		ca.cipher.Encrypt(ca._X[:], ca._X[:])
+		for k = 0; k < num; k++ {
+			ca._X[k] ^= buf[i]
 			i++
 		}
-                ca.count += num
-        }
+		ca.count += num
+	}
 }
 
 var cXor = [4]byte { 0x00, 0x87, 0x0E, 0x89 }
 
 func gf_mulx1(pad []byte) {
-        t := pad[0] >> 7
+	t := pad[0] >> 7
 
-        for i := 0; i < BLOCK_SIZE - 1; i++ {
-                pad[i] = (pad[i] << 1) | (pad[i + 1] >> 7)
+	for i := 0; i < BLOCK_SIZE - 1; i++ {
+		pad[i] = (pad[i] << 1) | (pad[i + 1] >> 7)
 	}
 
-        pad[BLOCK_SIZE - 1] = (pad[BLOCK_SIZE - 1] << 1) ^ cXor[t]
+	pad[BLOCK_SIZE - 1] = (pad[BLOCK_SIZE - 1] << 1) ^ cXor[t]
 }
 
 func gf_mulx2(pad []byte) {
-        t := pad[0] >> 6
+	t := pad[0] >> 6
 
-        for i := 0; i < BLOCK_SIZE - 1; i++ {
-                pad[i] = (pad[i] << 2) | (pad[i + 1] >> 6)
+	for i := 0; i < BLOCK_SIZE - 1; i++ {
+		pad[i] = (pad[i] << 2) | (pad[i + 1] >> 6)
 	}
 
-        pad[BLOCK_SIZE - 2] ^= (t >> 1)
-        pad[BLOCK_SIZE - 1] = (pad[BLOCK_SIZE - 1] << 2) ^ cXor[t]
+	pad[BLOCK_SIZE - 2] ^= (t >> 1)
+	pad[BLOCK_SIZE - 1] = (pad[BLOCK_SIZE - 1] << 2) ^ cXor[t]
 }
 
 func (ca *CmacCtx) Finish(mac []byte) {
 	var pad [BLOCK_SIZE]byte
 	ca.cipher.Encrypt(pad[:], pad[:])
 
-        k := ca.count % BLOCK_SIZE
-        if ca.count == 0 || k > 0 {
-                ca._X[k] ^= 0x80
-                gf_mulx2(pad[:])
-        } else {
-                gf_mulx1(pad[:])
-        }
+	k := ca.count % BLOCK_SIZE
+	if ca.count == 0 || k > 0 {
+		ca._X[k] ^= 0x80
+		gf_mulx2(pad[:])
+	} else {
+		gf_mulx1(pad[:])
+	}
 
-        blockXOR(pad[:], ca._X[:])
+	blockXOR(pad[:], ca._X[:])
 	ca.cipher.Encrypt(pad[:], pad[:])
 
 	copy(mac, pad[:])
@@ -115,8 +115,8 @@ func (ca *CmacCtx) Finish(mac []byte) {
 
 type EaxCtx struct {
 	cmac CmacCtx
-        encrypt bool
-        pos int
+	encrypt bool
+	pos int
 	_S [16]byte
 	_C [16]byte
 	_N [16]byte
@@ -132,24 +132,24 @@ func NewEax(cipher cipher.Block) (*EaxCtx, error) {
 }
 
 func (ax *EaxCtx) Start(encrypt bool, nonce, header []byte) {
-        ax.encrypt = encrypt
-        ax.pos = 0
+	ax.encrypt = encrypt
+	ax.pos = 0
 
 	var block [BLOCK_SIZE]byte
 
-        block[BLOCK_SIZE - 1] = 0
+	block[BLOCK_SIZE - 1] = 0
 	ax.cmac.Start()
 	ax.cmac.Update(block[:])
 	ax.cmac.Update(nonce)
 	ax.cmac.Finish(ax._N[:])
 
-        block[BLOCK_SIZE - 1] = 1
+	block[BLOCK_SIZE - 1] = 1
 	ax.cmac.Start()
 	ax.cmac.Update(block[:])
 	ax.cmac.Update(header)
 	ax.cmac.Finish(ax._H[:])
 
-        block[BLOCK_SIZE - 1] = 2
+	block[BLOCK_SIZE - 1] = 2
 	ax.cmac.Start()
 	ax.cmac.Update(block[:])
 
@@ -157,12 +157,12 @@ func (ax *EaxCtx) Start(encrypt bool, nonce, header []byte) {
 }
 
 func (ax *EaxCtx) increaseCounter() {
-        for i := BLOCK_SIZE - 1; i >= 0; i-- {
-                ax._C[i]++
-                if (ax._C[i] != 0) {
-                        break
+	for i := BLOCK_SIZE - 1; i >= 0; i-- {
+		ax._C[i]++
+		if (ax._C[i] != 0) {
+			break
 		}
-        }
+	}
 }
 
 func (ax *EaxCtx) Update(out, in []byte) {
@@ -194,30 +194,30 @@ func (ax *EaxCtx) Update(out, in []byte) {
 			dst[i] = ax._S[i] ^ src[i]
 		}
 		n += BLOCK_SIZE
-                num -= BLOCK_SIZE
+		num -= BLOCK_SIZE
 	}
 
-        if num > 0 {
+	if num > 0 {
 		ax.cmac.cipher.Encrypt(ax._S[:], ax._C[:])
 		ax.increaseCounter()
 
-                for ax.pos < num {
-                        out[n] = ax._S[ax.pos] ^ in[n]
+		for ax.pos < num {
+			out[n] = ax._S[ax.pos] ^ in[n]
 			n++
 			ax.pos++
-                }
-        }
+		}
+	}
 
-        if (ax.encrypt) {
-                ax.cmac.Update(out[:n])
+	if (ax.encrypt) {
+		ax.cmac.Update(out[:n])
 	}
 }
 
 func (ax *EaxCtx) Finish(mac []byte) {
 	ax.cmac.Finish(mac)
 
-        for i := 0; i < BLOCK_SIZE; i++ {
-                mac[i] ^= ax._N[i] ^ ax._H[i]
-        }
+	for i := 0; i < BLOCK_SIZE; i++ {
+		mac[i] ^= ax._N[i] ^ ax._H[i]
+	}
 }
 
