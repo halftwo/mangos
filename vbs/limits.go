b3ab448c53200061d263e7de23650366e4e601ac
halftwo <halftwo@u5i5.com>
2022-11-20 09:23:39 +0800

    Add const vbs.MaxInt64Length etc

--- vbs/limits.go
+++ vbs/limits.go
@@ -4,9 +4,21 @@ import (
 	"math"
 )
 
-var MaxLength int = 0	// <= 0 means no limits
+var MaxLength int = 0				// <= 0 means no limits
 
 var MaxStringLength int = math.MaxInt32
 
 var MaxDepth int16 = math.MaxInt16
+
+
+const MaxInt64Length = 1 + (63-5+6)/7		// 10
+
+const MaxUint32Length = 1 + (32-5+6)/7		// 5
+const MaxInt32Length = 1 + (31-5+6)/7		// 5
+
+const MaxUint16Length = 1 + (16-5+6)/7		// 3
+const MaxInt16Length = 1 + (15-5+6)/7		// 3
+
+const MaxUint8Length = 1 + (8-5+6)/7		// 2
+const MaxInt8Length = 1 + (7-5+6)/7		// 2
 
