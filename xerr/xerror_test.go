cde3a788a215f46f76b5c3ca77ee897150456ab7
halftwo <halftwo@u5i5.com>
2022-12-02 19:07:45 +0800

    fix test in xerr

--- xerr/xerror_test.go
+++ xerr/xerror_test.go
@@ -2,28 +2,35 @@ package xerr
 
 import (
 	"testing"
+	"io"
 )
 
 type MyError struct{}
 
+func (MyError) Error() string { return "#MyError#" }
+
 
 func foo() error {
-	return Trace(MyError{}, "my message")
+	x := MyError{}
+	return Trace(x, "my message")
 }
 
 func Test1(t *testing.T) {
+	xx := Trace(io.EOF)
+	t.Logf("%v", xx)
+
 	err1 := foo()
-	err2 := Trace(err1, "another message")
+	err2 := Tracef(err1, "another message")
 
 	if err1 != err2 {
-		panic("should be the same")
+		t.Fatal("should be the same")
 	}
 
 	switch err2.Cause().(type) {
 		case MyError:
 			break
 		default:
-			panic("can't be here")
+			t.Fatal("can't be here")
 	}
 
 	t.Logf("%#v", err1)
