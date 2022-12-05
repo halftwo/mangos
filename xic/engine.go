377f9de0c1bf40ac3a464148fd2b4c59447df255
halftwo <halftwo@u5i5.com>
2022-12-05 08:00:33 +0800

    refine error msg

--- xic/engine.go
+++ xic/engine.go
@@ -80,7 +80,7 @@ func newEngineSettingName(setting Setting, name string) *_Engine {
 	if shadow != "" {
 		engine.shadowBox, err = NewShadowBoxFromFile(shadow)
 		if err != nil {
-			dlog.Logf("XIC.WARN", "Failed to open shadow file %s", shadow)
+			dlog.Logf("XIC.WARN", "Failed to open shadow file %#s", shadow)
 		}
 	}
 
@@ -93,7 +93,7 @@ func newEngineSettingName(setting Setting, name string) *_Engine {
 	if secret != "" {
 		engine.secretBox, err = NewSecretBoxFromFile(secret)
 		if err != nil {
-			dlog.Logf("XIC.WARN", "Failed to open secret file %s", secret)
+			dlog.Logf("XIC.WARN", "Failed to open secret file %#s", secret)
 		}
 	}
 
@@ -156,7 +156,7 @@ func (engine *_Engine) CreateAdapterEndpoints(name string, endpoints string) (Ad
 		itemkey := name + ".Endpoints"
 		endpoints = engine.setting.Get(itemkey)
 		if endpoints == "" {
-			return nil, xerr.Errorf("%s not set", itemkey)
+			return nil, xerr.Errorf("%#s not given", itemkey)
 		}
 	}
 
